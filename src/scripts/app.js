269a29802a15adfc5e064912d8cf529c3296203b
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2024-04-23 13:07:08 +0200

    add css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,37 @@
-console.info('Hello world');
+import { gsap } from "gsap"; 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener('DOMContentLoaded', function () {
+    var section = document.getElementById('section__works');
+    var sectionHeight = section.scrollHeight;
+    section.style.height = sectionHeight + 'px';
+});
+
+
+
+var contenus = document.querySelectorAll(".contenu");
+
+document.addEventListener("scroll", function() {
+    
+    contenus.forEach(contenu => {
+
+        var masque = document.querySelector(".masque");
+    
+    
+        var masqueRect = masque.getBoundingClientRect();
+        var contenuRect = contenu.getBoundingClientRect();
+        
+        if (
+        contenuRect.bottom > masqueRect.top &&
+        contenuRect.top < masqueRect.bottom
+        ) {
+        gsap.to(contenu, { opacity: 0, duration: 0.2 });
+        } else {
+        gsap.to(contenu, { opacity: 1, duration: 0.3 });
+        }
+            
+    });
+    
+});
+  

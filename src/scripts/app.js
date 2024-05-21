c6f7f8c90f3a45b7f7f7d83f6af36251f095269b
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2024-05-21 17:10:53 +0200

    popup design

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 var contenus = document.querySelectorAll(".contenu");
 
+
 document.addEventListener("scroll", function() {
     
     contenus.forEach(contenu => {
@@ -23,43 +24,80 @@ document.addEventListener("scroll", function() {
         } else {
         gsap.to(contenu, { opacity: 1, duration: 0.3 });
         }
-            
+        
+           
     });
+
+
     
 });
+
+// var popup = document.querySelector(".popup");
+// popup.addEventListener("scroll",function(){
+
+//   var title = document.querySelector(".popup__title");
+//   var text = document.querySelector(".popup__info");
+//   var titleRect = title.getBoundingClientRect();
+//   var textRect = text.getBoundingClientRect();
+
+//   if (
+//     textRect.bottom > titleRect.top &&
+//     textRect.top < titleRect.bottom
+//     ) {
+//     gsap.to(text, { opacity: 0, duration: 0.2,y:90 });
+//     } 
+//     else {
+//     gsap.to(text, { opacity: 1, duration: 0.3,y:0 });
+//     }
+
+
+// })
 $(function() {
     $('.square').on('click', function() {
       var description = $(this).find('.description').text();
       var title = $(this).find('.title--popup').text();
+      var info = $(this).find('.info').text();
+      var info2 = $(this).find('.info2').text();
       $('.popup__title').text(title);
       $('.popup__description').text(description);
+      $('.popup__info').text(info);
+      $('.second_info').text(info2);
       $('.popup').show();
       
       // Ajouter une classe au corps pour figer le défilement
       $('body').addClass('popup-open');
     });
-  
-    $('.popup').on('click', function() {
-      $(this).hide();
-      
-      // Supprimer la classe du corps pour réactiver le défilement
-      $('body').removeClass('popup-open');
+
+
+    $('.popup__btn--close').on('click', function() {
+      $(this).closest('.popup').hide(); // Masquer le popup en trouvant le conteneur parent
+      $('body').removeClass('popup-open'); // Supprimer la classe du corps pour réactiver le défilement
     });
+
+    $('.popup__content').hide();
+    function toggleContent(targetId) {
+      var content = $('#' + targetId + '-content');
+      
+      if (content.is(':visible')) {
+          content.hide(); // Masquer le contenu s'il est déjà visible
+          
+      } else {
+          $('.popup__content').hide(); // Masquer tous les autres contenus
+          content.show(); // Afficher le contenu ciblé
+      }
+    }
+    
+    // Gérer les clics sur les boutons popup__btn
+    $('.popup__btn').on('click', function() {
+      var targetId = $(this).data('target'); // Récupérer l'ID cible à partir de l'attribut data-target
+      toggleContent(targetId); // Basculer l'affichage du contenu correspondant
+    });
+
 });
 
   
   
 
-// $(document).ready(function() {
-//     $('.contenu').each(function() {
-//       var contenuHeight = $(this).height(); // Hauteur du contenu
-//       var contenuWidth = $(this).width(); // Largeur du contenu
-      
-//       $(this).addClass('dynamic-height').css({
-//         'height': contenuHeight + 'px', // Définit la hauteur de .contenu à la hauteur de son contenu
-//         'width': contenuWidth + 'px' // Définit la largeur de .contenu à la largeur de son contenu
-//       });
-//     });
-//   });
+
   
   
--- www/index.php
+++ www/index.php
@@ -9,14 +9,34 @@ $template = $twig->load('index.twig');
 $projets_details = array(
   "Rux" => array(
       "description" => "This is the presentation of the redesign of our school playground ",
+      "info" => "At the request of the principal and teachers, we're going to help design the playground. 
+                It's not up to scratch yet, so we need to come up with ideas for its layout, including activities, 
+                furniture, etc.",
+        "info2" => "The difficulties here were not only coding or the design of the website it was to find the right ideas for
+                 the redesign of the playground, I had a lot more to think about. When a chose an idea I had to be able to make a 
+                 design for my website that reflected my ideas, that's tricky",
       "title" => "Rux",
       "images" => array(
           "/assets/images/rux_haut2.png",
           "/assets/images/rux_bas2.png"
-      )
+      ),
+      "design_images" => array(
+        "/assets/images/img_mobile-1.png",
+        "/assets/images/img_mobile-2.png",
+        "/assets/images/img_mobile-3.png"
+    )
+
   ),
   "Iolce" => array(
       "description" => "This was one of my first website. The task was to watch a conference “Ethics of web design” resume, comment and obviously make a website with these informations",
+      "infoo" => "The decisions weren't easy at all, the hardest for me was choosing the colors... 
+                To make everything compatible, readable and not too overloaded, it had to be the perfect match 
+                with the conference and the rest of the site. After a few tries, I chose dark red, white and black. 
+                To stay in the spirit of the conference.",
+        "info2" => "The decisions weren't easy at all, the hardest for me was choosing the colors... 
+        To make everything compatible, readable and not too overloaded, it had to be the perfect match 
+        with the conference and the rest of the site. After a few tries, I chose dark red, white and black. 
+        To stay in the spirit of the conference.",        
       "title" => "Iolce",
       "images" => array(
           "/assets/images/ethics.png"
